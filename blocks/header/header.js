cbc75ea2b21b80d5963016926047e6c63b1b1989
Celine Macke <cmacke121@gmail.com>
2023-10-23 18:13:34 -0400

    nav is now hoverable

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -113,13 +113,19 @@ export default async function decorate(block) {
     if (navSections) {
       navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
         if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-        navSection.addEventListener('click', () => {
+        navSection.addEventListener('mouseover', () => {
           if (isDesktop.matches) {
-            const expanded = navSection.getAttribute('aria-expanded') === 'true';
             toggleAllNavSections(navSections);
-            navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+            navSection.setAttribute('aria-expanded', 'true');
           }
         });
+        navSection.addEventListener('mouseout', () => {
+          if (isDesktop.matches) {
+            toggleAllNavSections(navSections);
+            navSection.setAttribute('aria-expanded', 'false');
+          }
+        });
+        
       });
     }
 
